0d85c3adb19c6682d52ce4dfea35503e161eb790
khangdpm225510-hash <khang_dpm225510@student.agu.edu.vn>
2025-10-06 09:56:25 +0700

    Add files via upload

---
+++ Mediator/Mediator_MyWorld/Real_Mediator_XinNghiPhep/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Real_Mediator_XinNghiPhep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator mediator = new ConcreteMediator();
+
+            QuanLy ql = new QuanLy(mediator, "Quan Ly A");
+            GiamDoc gd = new GiamDoc(mediator, "Giam Doc B");
+            NhanVien nv = new NhanVien(mediator, "Nhan Vien C", "NhanVien");
+
+            mediator.DangKyQuanLy(ql);
+            mediator.DangKyGiamDoc(gd);
+
+            Console.WriteLine("Nhan vien gui yeu cau nghi 1 ngay:");
+            nv.GuiYeuCau("Xin nghi 1 ngay");
+
+            Console.WriteLine("\nNhan vien gui yeu cau nghi 3 ngay:");
+            nv.GuiYeuCau("Xin nghi 3 ngay");
+        }
+    }
+}
---
+++ Mediator/Mediator_MyWorld/Real_Mediator_XinNghiPhep/QuanLy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Real_Mediator_XinNghiPhep
+{
+    public class QuanLy : NhanVien
+    {
+        public QuanLy(IMediator mediator, string ten) : base(mediator, ten, "QuanLy") { }
+
+        public void XuLyYeuCau(string yeuCau)
+        {
+            Console.WriteLine("Quan Ly xu ly yeu cau: " + yeuCau);
+            if (yeuCau.Contains("3 ngay"))
+            {
+                Console.WriteLine("Quan Ly: Yeu cau vuot qua quyen han, chuyen len Giam Doc");
+                mediator.GuiYeuCau(yeuCau, this);
+            }
+        }
+    }
+}
