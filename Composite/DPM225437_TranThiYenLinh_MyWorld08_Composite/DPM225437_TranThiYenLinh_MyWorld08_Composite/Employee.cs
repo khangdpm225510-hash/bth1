10c557e1f5c5aefce44057ec6a22379910f314e2
khangdpm225510-hash <khang_dpm225510@student.agu.edu.vn>
2025-10-06 08:50:29 +0700

    Add files via upload

---
+++ Composite/DPM225437_TranThiYenLinh_MyWorld08_Composite/DPM225437_TranThiYenLinh_MyWorld08_Composite/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225437_TranThiYenLinh_MyWorld08_Composite
+{
+    public class Employee : CompanyComponent
+    {
+        public Employee(string name) : base(name) { }
+
+        public override void Add(CompanyComponent c)
+        {
+            Console.WriteLine("Không thể thêm vào nhân viên.");
+        }
+
+        public override void Remove(CompanyComponent c)
+        {
+            Console.WriteLine("Không thể xóa trong nhân viên.");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + " Nhân viên: " + name);
+        }
+    }
+}
---
+++ Composite/DPM225437_TranThiYenLinh_Pattern08_Composite/DPM225437_TranThiYenLinh_Pattern08_Composite/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DPM225437_TranThiYenLinh_Pattern08_Composite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DPM225437_TranThiYenLinh_Pattern08_Composite")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f7d2134-5566-4ef1-8805-078e2773e521")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
