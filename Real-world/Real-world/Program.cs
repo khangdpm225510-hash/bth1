8116d41c8d73a429a655b585673c5e91e12e112c
khangdpm225510-hash <khang_dpm225510@student.agu.edu.vn>
2025-10-06 08:09:20 +0700

    Add files via upload

---
+++ Real-world/Real-world/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_world
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Non-adapted chemical compound
+            Compound unknown = new Compound();
+            unknown.Display();
+
+            // Adapted chemical compounds
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
